7669c3affc0afee1bef52bcfc78a4b4e9e4b26e2
Roger <rogerruizsalvat@gmail.com>
2022-12-11 20:56:46 +0100

    fixed urls

--- src/main/resources/static/js/upload-video.js
+++ src/main/resources/static/js/upload-video.js
@@ -4,16 +4,16 @@ videoForm.addEventListener("submit", ev => {
     ev.preventDefault();
     let data = new FormData(videoForm);
 
-    fetch("http://localhost:8080/video", {
+    fetch("video", {
         method: "POST",
         body: data
     }).then(response => {
         var xmlHttp = new XMLHttpRequest();
 
         if (response.status === 200) {
-            xmlHttp.open( "GET", "http://localhost:8080/components/video-upload-success", false);
+            xmlHttp.open( "GET", "components/video-upload-success", false);
         } else {
-            xmlHttp.open( "GET", "http://localhost:8080/components/video-upload-fail", false);
+            xmlHttp.open( "GET", "components/video-upload-fail", false);
         }
 
         xmlHttp.send( null );
--- src/main/resources/static/page/script.js
+++ src/main/resources/static/page/script.js
@@ -6,17 +6,17 @@ const videoDescription = document.querySelector("#video-description");
 const queryParams = Object.fromEntries(new URLSearchParams(window.location.search));
 
 if(queryParams.video){
-    videoScreen.src = `http://localhost:8080/video/${queryParams.video}`;
+    videoScreen.src = `../video/${queryParams.video}`;
     videoPlayer.style.display = "block";
     videoTitle.innerText = "Now playing " + queryParams.video;
 }
 
-fetch(`http://localhost:8080/video/description/${queryParams.video}`).then(response => {
+fetch(`../video/description/${queryParams.video}`).then(response => {
     response.text().then(responseText => videoDescription.innerText = responseText);
 });
 
 // components
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "http://localhost:8080/components/title-bar", false);
+xmlHttp.open( "GET", "../components/title-bar", false);
 xmlHttp.send( null );
 document.getElementById("title-bar").innerHTML=xmlHttp.responseText;
