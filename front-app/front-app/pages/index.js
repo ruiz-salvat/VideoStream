62519a5f1d9ac3d9a29be847c3526f343bdcca01
Roger <rogerruizsalvat@gmail.com>
2023-06-28 18:35:42 +0200

    test button

--- front-app/front-app/pages/index.js
+++ front-app/front-app/pages/index.js
@@ -2,8 +2,19 @@ import Layout from '../components/layout'
 import CategoryContainer from '../components/index/category_container'
 
 export default function HomePage({ categories, videos }) {
+   function testButton() {
+      fetch(`${process.env.NEXT_PUBLIC_LOCALHOST_URL}video/details/yufgh`)
+      .then(response => response.json())
+      .then(response => {
+         console.log("Response", response);
+      })
+   }
+
    return (
       <Layout>
+            <div>
+               <button onClick={testButton}>Test Button</button>
+            </div>
             <div>
                {categories.map((category) => (
                   <CategoryContainer key={category.id} category={category} videos={videos.filter(video => video.category === category.id)}/>
