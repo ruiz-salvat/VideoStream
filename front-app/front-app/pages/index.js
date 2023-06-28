374c3391ea64bd9ec538833ea3bb0de3a97b3357
Roger <rogerruizsalvat@gmail.com>
2023-06-28 21:28:27 +0200

    url

--- front-app/front-app/pages/index.js
+++ front-app/front-app/pages/index.js
@@ -3,7 +3,7 @@ import CategoryContainer from '../components/index/category_container'
 
 export default function HomePage({ categories, videos }) {
    function testButton() {
-      fetch(`${process.env.NEXT_PUBLIC_LOCALHOST_URL}video/details/sinkin_bathtub`, {
+      fetch(`video/details/sinkin_bathtub`, {
          credentials: "include",
          mode: "cors",
          headers: {
